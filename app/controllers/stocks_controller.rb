da77a8c063076adf4ae94815e7946e62653de52c
Mashrur Hossain <mashrur.hossain@gmail.com>
2020-01-07 22:08:29 +0100

    Add ajax functionality for searching stocks and handling errors

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -4,15 +4,20 @@ class StocksController < ApplicationController
     if params[:stock].present?
       @stock = Stock.new_lookup(params[:stock])
       if @stock
-        render 'users/my_portfolio'
+        respond_to do |format|
+          format.js { render partial: 'users/result' }
+        end
       else
-        flash[:alert] = "Please enter a valid symbol to search"
-        redirect_to my_portfolio_path
+        respond_to do |format|
+          flash.now[:alert] = "Please enter a valid symbol to search"
+          format.js { render partial: 'users/result' }
+        end
       end    
     else
-      flash[:alert] = "Please enter a symbol to search"
-      redirect_to my_portfolio_path
+      respond_to do |format|
+        flash.now[:alert] = "Please enter a symbol to search"
+        format.js { render partial: 'users/result' }
+      end
     end
   end
-
 end
