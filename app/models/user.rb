daa379099a7823ee50b03d7e1940579f852f0e94
Mashrur Hossain <mashrur.hossain@gmail.com>
2020-01-11 23:18:55 +0100

    Add stock tracking restrictions for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def stock_already_tracked?(ticker_symbol)
+    stock = Stock.check_db(ticker_symbol)
+    return false unless stock
+    stocks.where(id: stock.id).exists?    
+  end
+  
+  def under_stock_limit?
+    stocks.count < 10
+  end
+
+  def can_track_stock?(ticker_symbol)
+    under_stock_limit? && !stock_already_tracked?(ticker_symbol)
+  end
 end
