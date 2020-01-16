1cd58234a43c9ff3a03c8bd5b23a7463155554f6
Mashrur Hossain <mashrur.hossain@gmail.com>
2020-01-16 13:18:06 +0100

    Add my friends view with listing of followed friends

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
+
   def my_portfolio
     @tracked_stocks = current_user.stocks
+  end
+
+  def my_friends
+    @friends = current_user.friends
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stock', to: 'stocks#search'
+  get 'my_friends', to: 'users#my_friends'
 end
