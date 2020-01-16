e0ff8fc414cb1d632b4be4b3e3afc9aa98562359
Mashrur Hossain <mashrur.hossain@gmail.com>
2020-01-16 15:50:32 +0100

    Search friends Ajax submission and JS response

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,23 @@ class UsersController < ApplicationController
   end
 
   def search
-    render json: params[:friend] 
+    if params[:friend].present?
+      @friend = params[:friend]
+      if @friend
+        respond_to do |format|
+          format.js { render partial: 'users/friend_result' }
+        end
+      else
+        respond_to do |format|
+          flash.now[:alert] = "Couldn't find user"
+          format.js { render partial: 'users/friend_result' }
+        end
+      end    
+    else
+      respond_to do |format|
+        flash.now[:alert] = "Please enter a friend name or email to search"
+        format.js { render partial: 'users/friend_result' }
+      end
+    end
   end
 end
